707dc2debaa9de9de02f2c2f6efed2494604e12e
Muhammad Husni Nur Fadillah <muhammadhusninurfadillah@gmail.com>
2023-04-03 01:32:42 +0700

    feat(app): 1 created a navbar (#10)
    Just installed some dependecies @headlessui/react and react-icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Navbar from '@/components/organisms/Navbar'
 
 export default function Home() {
     return (
@@ -19,7 +16,12 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <h1 className="text-red-500">hello</h1>
+
+            <div className='container mx-auto'>
+                <Navbar />
+            </div>
+
+
         </>
     )
 }
