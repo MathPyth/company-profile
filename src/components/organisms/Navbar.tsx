d5aea695897e6b0822a096da03970673b4b0068c
Ananda Rizky Ramadhan <79091335+anandarizkyrm@users.noreply.github.com>
2023-04-05 18:26:54 +0700

    Page fox (#23)
    * tailwind config
    
    * jumbotron
    
    * change some
    
    * fix page ssr and rafactor layout
    
    * delete

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import { Popover, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import React from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from 'react'
 
-import { GiHamburgerMenu } from "react-icons/gi";
-import { AiOutlineClose } from "react-icons/ai";
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 import Image from 'next/image'
 
@@ -11,33 +11,35 @@ const navigations = [
     { name: 'Home', href: '#' },
     { name: 'About', href: '#' },
     { name: 'Project', href: '#' },
-    { name: 'Contact', href: '#' }
-];
+    { name: 'Contact', href: '#' },
+]
 
 const Navbar = () => {
     return (
         <>
             <svg
-                className='absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block'
-                fill='currentColor'
-                viewBox='0 0 100 100'
-                preserveAspectRatio='none'
-                aria-hidden='true'
+                className="absolute inset-y-0 right-0 hidden  h-full w-48 translate-x-1/2 transform text-white lg:hidden"
+                fill="currentColor"
+                viewBox="0 0 100 100"
+                preserveAspectRatio="none"
+                aria-hidden="true"
             >
-                <polygon points='50,0 100,0 50,100 0,100' />
+                <polygon points="50,0 100,0 50,100 0,100" />
             </svg>
 
             <Popover>
-                <div className='relative px-4 pt-6 sm:px-6 lg:px-8'>
+                <div className="relative px-4 pt-6 sm:px-6 lg:px-8">
                     <nav
-                        className='relative flex items-center justify-between sm:h-10 lg:justify-start'
-                        aria-label='Global'
+                        className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                        aria-label="Global"
                     >
-                        <div className='flex flex-shrink-0 flex-grow items-center lg:flex-grow-0'>
-                            <div className='flex w-full items-center justify-between md:w-auto'>
-                                <a href='#'>
-                                    <span className='sr-only'>Your Company</span>
-                                    <div className='text-lg font-bold text-gray-400'>
+                        <div className="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
+                            <div className="flex w-full items-center justify-between md:w-auto">
+                                <a href="#">
+                                    <span className="sr-only">
+                                        Your Company
+                                    </span>
+                                    <div className="text-lg font-bold text-gray-400">
                                         <Image
                                             src="/mathpy.png"
                                             width={70}
@@ -46,20 +48,22 @@ const Navbar = () => {
                                         />
                                     </div>
                                 </a>
-                                <div className='-mr-2 flex items-center md:hidden'>
-                                    <Popover.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
-                                        <span className='sr-only'>Open main menu</span>
+                                <div className="-mr-2 flex items-center md:hidden">
+                                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">
+                                            Open main menu
+                                        </span>
                                         <GiHamburgerMenu />
                                     </Popover.Button>
                                 </div>
                             </div>
                         </div>
-                        <div className='hidden md:ml-10 md:block md:space-x-8 md:pr-4'>
+                        <div className="hidden md:ml-10 md:block md:space-x-8 md:pr-4">
                             {navigations.map((item) => (
                                 <a
                                     key={item.name}
                                     href={item.href}
-                                    className='font-medium text-gray-500 hover:text-gray-900'
+                                    className="font-medium text-gray-500 hover:text-gray-900"
                                 >
                                     {item.name}
                                 </a>
@@ -70,37 +74,39 @@ const Navbar = () => {
 
                 <Transition
                     as={Fragment}
-                    enter='duration-150 ease-out'
-                    enterFrom='opacity-0 scale-95'
-                    enterTo='opacity-100 scale-100'
-                    leave='duration-100 ease-in'
-                    leaveFrom='opacity-100 scale-100'
-                    leaveTo='opacity-0 scale-95'
+                    enter="duration-150 ease-out"
+                    enterFrom="opacity-0 scale-95"
+                    enterTo="opacity-100 scale-100"
+                    leave="duration-100 ease-in"
+                    leaveFrom="opacity-100 scale-100"
+                    leaveTo="opacity-0 scale-95"
                 >
                     <Popover.Panel
                         focus
-                        className='absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden'
+                        className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
                     >
-                        <div className='overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5'>
-                            <div className='flex items-center justify-between px-5 pt-4'>
+                        <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+                            <div className="flex items-center justify-between px-5 pt-4">
                                 <div>
-                                    <div className='text-lg font-bold text-gray-400'>
+                                    <div className="text-lg font-bold text-gray-400">
                                         MathPy
                                     </div>
                                 </div>
-                                <div className='-mr-2'>
-                                    <Popover.Button className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
-                                        <span className='sr-only'>Close main menu</span>
+                                <div className="-mr-2">
+                                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="sr-only">
+                                            Close main menu
+                                        </span>
                                         <AiOutlineClose />
                                     </Popover.Button>
                                 </div>
                             </div>
-                            <div className='space-y-1 px-2 pt-2 pb-3'>
+                            <div className="space-y-1 px-2 pt-2 pb-3">
                                 {navigations.map((item) => (
                                     <a
                                         key={item.name}
                                         href={item.href}
-                                        className='block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900'
+                                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
                                     >
                                         {item.name}
                                     </a>
@@ -114,4 +120,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default Navbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-
-        // Or if using `src` directory:
-        './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-        extend: {
-            colors: {
-                mathpylue: '#2463EA',
-                mathpyDark: '#0F172A',
-                mathpyWhite: '#FFFEFE',
-                mathpygray: '#8A97A9',
-            },
-            fontSize: {
-                smallest: '0.875rem',
-                small: '1rem',
-                base: '1.25rem',
-                large: '2.5rem',
-                extralarge: '3rem',
-            },
-        },
-    },
-    plugins: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
+
 
