0f123031076ac733dc19290f69826d73b82ea326
anandarizkyrm <rizky.pky@gmail.com>
2023-04-05 13:48:42 +0700

    change some

---
+++ src/components/template/Layout.tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import React from 'react'
+import { Inter } from 'next/font/google'
+import htmlhead from '../../../json/htmlhead.json'
+import Navbar from '../organisms/Navbar'
+
+type Props = {
+    children: React.ReactNode
+}
+const roboto = Inter({
+    weight: ['400', '700'],
+    style: ['normal'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+const Layout = (props: Props) => {
+    const { children } = props
+    return (
+        <>
+            <Head>
+                <title>{htmlhead.title}</title>
+                <meta name={htmlhead.title} content={htmlhead.content} />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Navbar />
+            <main className={roboto.className}>{children}</main>
+        </>
+    )
+}
+
+export default Layout
