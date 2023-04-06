61432ecfad262da3bb1f2dfc06a3eb8582a14f6e
anandarizkyrm <rizky.pky@gmail.com>
2023-04-06 12:01:39 +0700

    all set

--- src/components/organisms/sections/Porfofolio.tsx
+++ src/components/organisms/sections/Porfofolio.tsx
@@ -8,20 +8,40 @@ const PortoList = [
         title: 'Chat Gpt',
         descrition:
             'ChatGPT is a sibling model to InstructGPT, which is trained to follow an instruction in a prompt and provide a detailed response.',
-        img: '/mathpy.png',
+        img: '/ss.png',
     },
     {
         title: 'FaceBook',
         descrition:
             'ChatGPT is a sibling model to InstructGPT, which is trained to follow an instruction in a prompt and provide a detailed response.',
-        img: '/mathpy.png',
+        img: '/ss.png',
         whiteCard: true,
     },
     {
         title: 'Twitter',
         descrition:
             'ChatGPT is a sibling model to InstructGPT, which is trained to follow an instruction in a prompt and provide a detailed response.',
-        img: '/mathpy.png',
+        img: '/ss.png',
+    },
+    {
+        title: 'Chat Gpt',
+        descrition:
+            'ChatGPT is a sibling model to InstructGPT, which is trained to follow an instruction in a prompt and provide a detailed response.',
+        img: '/ss.png',
+        whiteCard: true,
+    },
+    {
+        title: 'FaceBook',
+        descrition:
+            'ChatGPT is a sibling model to InstructGPT, which is trained to follow an instruction in a prompt and provide a detailed response.',
+        img: '/ss.png',
+    },
+    {
+        title: 'Twitter',
+        descrition:
+            'ChatGPT is a sibling model to InstructGPT, which is trained to follow an instruction in a prompt and provide a detailed response.',
+        img: '/ss.png',
+        whiteCard: true,
     },
 ]
 
@@ -51,14 +71,21 @@ const Porfofolio = () => {
                     <Card
                         key={porto.title}
                         color={porto.whiteCard ? 'light' : 'dark'}
-                        className="text-left"
+                        className="text-left mt-12"
                     >
+                        <Image
+                            alt="Porto"
+                            src={porto.img}
+                            width={300}
+                            height={300}
+                            className="rounded-xl mb-6"
+                        ></Image>
                         <Typography
-                            size={'large'}
+                            size={'medium'}
                             fontFamily={'lexend'}
                             color={porto.whiteCard ? 'secondary' : 'white'}
-                            fontWeight="light"
-                            className="text-4xl font-bold"
+                            fontWeight="bold"
+                            className="text-4xl"
                         >
                             {porto.title}
                         </Typography>
@@ -67,17 +94,9 @@ const Porfofolio = () => {
                             fontFamily={'lexend'}
                             color={'light'}
                             fontWeight="semi-bold"
-                            className="mt-4"
                         >
                             {porto.descrition}
                         </Typography>
-                        <Image
-                            alt="Porto"
-                            src="/mathpy.png"
-                            width={300}
-                            height={300}
-                            className="rounded-xl mt-6"
-                        ></Image>
                     </Card>
                 ))}
             </div>
