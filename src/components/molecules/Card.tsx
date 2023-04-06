11f764a5f0ae4541cb3295343e187b45c9640295
husfuu <muhammadhusninurfadillah@gmail.com>
2023-04-06 14:37:14 +0700

    add props

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -1,20 +1,30 @@
 import React from "react";
+import Typography from "../atoms/Typography";
 
+interface CardItemProps {
+    title: string,
+    description: string
+    image: string
+}
 
-const Card = () => {
+const Card = (props: CardItemProps) => {
+    const {
+        title, description, image
+    } = props;
+
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src="https://placeimg.com/400/225/arch" alt="Sunset in the mountains" />
+            <img className="w-full" src={image} alt="Sunset in the mountains" />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                <p className="text-gray">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                </p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#machine-learning</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#web</span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#mobile</span>
+                <div className="font-bold text-xl mb-2">{title}</div>
+                <Typography
+                    size={'small'}
+                    fontFamily={'inter'}
+                    color={'light'}
+                    fontWeight="light"
+                >
+                    {description}
+                </Typography>
             </div>
         </div>
     )
